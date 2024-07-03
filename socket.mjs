0e96211ba32c5533a8c1c4610911621a85a38bb0
vicky-at-web <144013983+vicky-at-web@users.noreply.github.com>
2024-07-03 20:53:14 +0530

    like feature update

--- socket.mjs
+++ socket.mjs
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
 import Campground from './models/campground.mjs'
+import User from './models/user.mjs';
 
 let io;
 
@@ -9,22 +10,35 @@ function initializeSocket(server) {
     console.log('Socket io connected');
 
 
-   
+
 
     io.on('connection', (socket) => {
         console.log('Socket connected:', socket.id);
 
-    socket.on('addLike', async data =>{
+        socket.on('addLike', async data => {
             console.log(data)
-            const {user, campground } = data
+            const { user, campground } = data
             const campground2 = await Campground.findById(campground);
-            if(campground2.likes.likedBy.includes(data.user)){
-                socket.emit('likeError', 'You cannot like the post again after liking it once.');
-            }else{
-                campground2.likes.totalLikes = campground2.likes.totalLikes + 1;
-                campground2.likes.likedBy.push(user);
-                await campground2.save();
+            campground2.likes.totalLikes++;
+            campground2.likes.likedBy.push(user);
+            await campground2.save();
+
+            socket.emit('likeCount', campground2.likes.totalLikes)
+        })
+
+        socket.on('dislike', async data => {
+            console.log(data)
+            const { user, campground } = data
+            const campground2 = await Campground.findById(campground);
+            const filteredArray = campground2.likes.likedBy.filter(likedUser => likedUser.id !== user)
+            console.log({ filteredArray: filteredArray })
+            campground2.likes.likedBy = filteredArray
+            if (campground2.likes.totalLikes > 0) {
+                campground2.likes.totalLikes--;
+            } else {
+                campground2.likes.totalLikes = 0
             }
+            await campground2.save();
             socket.emit('likeCount', campground2.likes.totalLikes)
         })
 
