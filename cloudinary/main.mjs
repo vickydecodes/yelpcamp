97a3281e2cd359388df94b58762708f375b32169
vicky-at-web <144013983+vicky-at-web@users.noreply.github.com>
2024-07-19 09:38:05 +0530

    Update main.mjs

--- cloudinary/main.mjs
+++ cloudinary/main.mjs
@@ -1,14 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_KEY || !process.env.CLOUDINARY_SECRET) {
-    throw new Error('Cloudinary configuration error: Missing necessary environment variables.');
-}
-
-// Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
