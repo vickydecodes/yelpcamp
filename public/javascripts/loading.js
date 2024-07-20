be170eb03ee433fabb81b4410ec8bf167fff3074
vicky-at-web <144013983+vicky-at-web@users.noreply.github.com>
2024-07-20 10:16:53 +0530

    final commit

---
+++ public/javascripts/loading.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("load", function () {
+    document.querySelector('.spinner-grow').style.display = 'none';
+    document.querySelector('body').style.opacity = 1;
+});
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('.spinner-grow').style.display = 'none';
+    document.querySelector('body').style.opacity = 1;
+})
